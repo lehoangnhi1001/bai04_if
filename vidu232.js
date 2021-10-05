bb7692d2ebca4c61347f4357cd7bbd37eb26d743
lehoangnhi1001 <lehoangnhi10012000@hi10012000gmail.12000gmailcom>
2021-10-05 23:08:19 +0700

    first commit

---
+++ vidu232.js
@@ -0,0 +1,9 @@
+var number = parseInt(prompt("Nhập số cần kiểm tra"));
+var mod = (number % 2);
+if (mod == 0){
+  document.write(number + " là số chẵn");
+} else if (mod == 1){
+  document.write(number + " là số lẻ");
+} else {
+  document.write("Ký tự bạn nhập không phải số");
+}
